55482d136381830a9a85afba5afcf1d2c7ff1794
Pascal Sthamer <10992664+P4sca1@users.noreply.github.com>
2024-07-18 16:36:13 +0200

    Correctly template helix dependency install script and only run on MacOS
    Script for Windows without Bash is WIP

--- run_onchange_helix-install-dependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Rerun this script whenever the pnpm-lock.yaml file changes
-# pnpm-lock.yaml hash: {{ include dot_config/helix/pnpm-lock.yaml | sha256sum }}
-pnpm --dir {{ joinPath .chezmoi.sourceDir "dot_config/helix" | quote }} install
