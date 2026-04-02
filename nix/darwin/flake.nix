e10f23d683e84772492f61fa431b52d7ef8c2385
Pascal Sthamer <pascal+github@sthamer.xyz>
2026-04-02 15:40:48 +0200

    add alacritty

--- nix/darwin/flake.nix
+++ nix/darwin/flake.nix
@@ -183,7 +183,15 @@
             programs.alacritty = {
               enable = true;
               package = pkgs.alacritty;
-              settings = {};
+              settings = {
+                font = {
+                  normal = { family = "JetBrainsMono Nerd Font Mono"; style = "Regular"; };
+                  bold = { family = "JetBrainsMono Nerd Font Mono"; style = "Bold"; };
+                  italic = { family = "JetBrainsMono Nerd Font Mono"; style = "Italic"; };
+                  bold_italic = { family = "JetBrainsMono Nerd Font Mono"; style = "Bold Italic"; };
+                  size = 13;
+                };
+              };
             };
 
             programs.bat = {
